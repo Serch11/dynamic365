6c408dafc906c22b769f856e8cff08d0babfa473
Serch <sredondo95@gmail.com>
2021-05-21 23:05:34 -0500

    1

--- divisa.js
+++ divisa.js
@@ -1,8 +1,6 @@
 function obtenerTrm(executeContext) {
+
   let formContext = executeContext.getFormContext();
-
-  let transactioncurrencyid = formContext.getAttribute("transactioncurrencyid");
-
   formContext.getAttribute("transactioncurrencyid").addOnChange(ObtenerDivisa);
 }
 
@@ -13,6 +11,7 @@ function ObtenerDivisa(executeContext) {
   let transactioncurrencyid = formContext.getAttribute("transactioncurrencyid");
 
   if (transactioncurrencyid.getValue()) {
+    alert( transactioncurrencyid.getValue()[0].name);
     if (transactioncurrencyid.getValue()[0].name === "US Dollar") {
 
       const datosAcceso = new FormData();
@@ -46,21 +45,7 @@ function ObtenerDivisa(executeContext) {
 
 
 
-      //xrm.addEventListener("readystatechange",(e))
 
-
-      //   req.onreadystatechange = function(aEvt) {
-      //     if (req.readyState == 4) {
-
-      //         console.log(aEvt);
-      //     }
-      //   };
-      //   xml.setRequestHeader("Content-Type", "application/json");
-      //   xml.open("POST", "http://192.168.30.187/D365/oferta/trm", {
-      //     username: "prueba",
-      //     password: "123"
-      //   });
-      //   xml.send();
     }
   }
 }
