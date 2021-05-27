08fb8cee6fc56bba80c90e7e0e070ea770a85618
Sergio Andres Redondo Aycardi <ingdesarrollo13@redsis.com>
2021-05-27 08:00:19 -0500

    v2

--- divisa.js
+++ divisa.js
@@ -1,17 +1,29 @@
 function obtenerTrm(executeContext) {
   let formContext = executeContext.getFormContext();
-  formContext.getAttribute("transactioncurrencyid").addOnChange(obtenerDivisa);
+
+  //formContext.getAttribute("transactioncurrencyid").addOnChange(obtenerDivisa);
+  console.log(executeContext.getDepth() + " getdepth 1");
+  formContext.data.removeOnLoad(removerFuncion);
 }
 
 function obtenerDivisa(executeContext) {
+  console.log(executeContext.getDepth() + " getdepth  2");
   let formContext = executeContext.getFormContext();
   let transactioncurrencyid = formContext.getAttribute("transactioncurrencyid");
   let ap_trm = formContext.getAttribute("ap_trm");
+  console.log(formContext.ui.formSelector.getCurrentItem());
 
   if (transactioncurrencyid.getValue()) {
-    if (transactioncurrencyid.getValue()[0].name === "US Dollar") {
+    if (
+      transactioncurrencyid.getValue()[0].name === "US Dollar" &&
+      (executeContext.getDepth() === 1 ||
+        executeContext.getDepth() === 2 ||
+        executeContext.getDepth() === 0)
+    ) {
       console.log(transactioncurrencyid.getValue()[0].name);
-
+      console.log(
+        executeContext.getDepth() + " getdepth  entro a las dos condiciones"
+      );
       Xrm.WebApi
         .retrieveMultipleRecords(
           "transactioncurrency",
@@ -22,6 +34,12 @@ function obtenerDivisa(executeContext) {
             console.log(result);
             if (result.entities[0].exchangerate) {
               let trm = result.entities[0].exchangerate;
+              console.log("hizo el cambio de la divisa");
+              console.log(
+                executeContext.getDepth() +
+                  " getdepth  entro a las dos condiciones y al resultado"
+              );
+              console.log(executeContext.getDepth() + " getdepth 2");
               ap_trm.setValue(trm);
               // perform additional operations on retrieved records
             }
@@ -31,8 +49,17 @@ function obtenerDivisa(executeContext) {
             // handle error conditions
           }
         );
-    } else {
+    }
+
+    if (transactioncurrencyid.getValue()[0].name != "US Dollar") {
       ap_trm.setValue(null);
     }
   }
+}
+
+function removerFuncion(executeContext) {
+  console.log(executeContext.getDepth() + " getdepth  3");
+  console.log("entro a remover la function");
+  let formContext = executeContext.getFormContext();
+  formContext.getAttribute("transactioncurrencyid").addOnChange(obtenerDivisa);
 }
