5fd983e0d90aa729d287e1cc519e6ede3313db82
Sergio Andres Redondo Aycardi <ingdesarrollo13@redsis.com>
2021-06-10 18:01:34 -0500

    09-06-2021

---
+++ getContactAndAccountIncident.js
@@ -0,0 +1,40 @@
+function getContactAndAccount(executionContext) {
+  let formContext = executionContext.getFormContext();
+
+  formContext.getAttribute("customerid").addOnChange(listentChange);
+}
+
+async function listentChange(executionContext) {
+  let formContext = executionContext.getFormContext();
+  let customerid = formContext.getAttribute("customerid");
+  let ap_valordelestado = formContext.getAttribute("ap_valordelestado");
+
+  if (customerid.getValue()) {
+    console.log(customerid.getValue());
+
+    let entidad = customerid.getValue()[0].entityType;
+    let id = customerid.getValue()[0].id;
+    let options = "?$select=statecode,statuscode";
+
+    try {
+      let result = await Xrm.WebApi.retrieveRecord(entidad, id, options);
+      console.log(result);
+      let statuscode = await result.statuscode;
+
+      statuscode === 1
+        ? ap_valordelestado.setValue(778210000)
+        : ap_valordelestado.setValue(778210001);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  if (
+    !customerid.getValue() &&
+    (executionContext.getDepth() === 0 ||
+      executionContext.getDepth() === 1 ||
+      executionContext.getDepth() === 3)
+  ) {
+    ap_valordelestado.setValue(null);
+  }
+}
