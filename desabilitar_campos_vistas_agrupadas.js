044b27087ffaa7cdd55eec723f6aa947aa9af013
Serch <sredondo95@gmail.com>
2021-09-12 21:00:10 -0500

    09/09/2021

--- desabilitar_campos_vistas_agrupadas.js
+++ desabilitar_campos_vistas_agrupadas.js
@@ -8,7 +8,11 @@ function onRecordSelect(exeContext) {
         data.push(value._attributeName);
     });
     //console.log(data);
+    
+    
     var disableFields = ["ap_exhibitpwrst", "ownerid", "parentaccountid", "estimatedvalue"];
+
+
     lockFields(exeContext, data);
 }
 
@@ -16,23 +20,11 @@ function lockFields(exeContext, disableFields) {
     var _formContext = exeContext.getFormContext();
     var currentEntity = _formContext.data.entity;
     currentEntity.attributes.forEach(function (attribute, i) {
-        console.log(attribute.getName());
+        //console.log(attribute.getName());
         if (disableFields.indexOf(attribute.getName()) > -1) {
-            console.log(attribute.controls.get(0));
+            //console.log(attribute.controls.get(0));
             var attributeToDisable = attribute.controls.get(0);
             attributeToDisable.setDisabled(true);
         }
     });
-}
-
-
-function prueba(executionContext) {
-    console.log(executionContext);
-    var cs = Xrm.Page.ui.controls.get();
-    for (var i in cs) {
-        var c = cs[i];
-        if (c.getName() != "" && c.getName() != null) {
-            if (!c.getDisabled()) { c.setDisabled(true); }
-        }
-    }
 }
--- estadoPedido.js
+++ estadoPedido.js
@@ -1,7 +1,7 @@
 function estadoPedido(executeContext) {
   let formContext = executeContext.getFormContext();
   //formContext.data.process.addOnProcessStatusChange(cambioEstado);
-  
+
 
   //estos roles van a poder editar el pedido cuando el estado este en elaboracion
   //RCO - Asistente de ventas
@@ -24,96 +24,90 @@ function estadoPedido(executeContext) {
   //detenido en compras
   //RCO - Compras 
   //se le habilitan todos los campos
-  
+
 
   //pendiente por recibir
   //RCO - Almacén
 
 
-
   //pendiente por facturar
-//RCO - Facturación
-
+  //RCO - Facturación
 
 
   //factura parcial
-//RCO - Facturación
-
+  //RCO - Facturación
 
   //factura
-//RCO - Facturación
-
-
-
-
-
+  //RCO - Facturación
   formContext.data.process.addOnStageChange(cambioEstado);
-
   //rol prueba
 }
 function cambioEstado(executeContext) {
-  let formContext = executeContext.getFormContext();
-  var objEstado = formContext.data.process.getActiveStage();
-  var estado = formContext.getAttribute("statuscode");
-  var estate = formContext.getAttribute("billto_line3");
-  var fase = objEstado.getName();
-  console.clear();
-  //alert("JS Estado Pedido: Fase = "+fase);
-  console.log("JS Estado Pedido: Fase = " + fase);
 
-  if (fase === "En Elaboración") {
-    estado.setValue(1);
-    estate.setValue("1");
-    Xrm.Page.data.entity.save();
-  }
-  if (fase === "En Revisión") {
-    estado.setValue(2);
-    estate.setValue("2");
-    Xrm.Page.data.entity.save();
-  }
-  if (fase === "Área Operativa") {
-    estado.setValue(778210005);
-    estate.setValue("778210005");
-    Xrm.Page.data.entity.save();
-  }
-  if (fase === "Pendiente Por Compra") {
-    estado.setValue(778210000);
-    estate.setValue("778210000");
-    Xrm.Page.data.entity.save();
-  }
-  if (fase === "Detenido En Compras") {
-    estado.setValue(778210001);
-    estate.setValue("778210001");
-    Xrm.Page.data.entity.save();
-  }
-  if (fase === "Pendiente Por Recibir") {
-    estado.setValue(778210002);
-    estate.setValue("778210002");
-    Xrm.Page.data.entity.save();
-  }
-  if (fase === "Pendiente Por Facturar") {
-    estado.setValue(778210003);
-    estate.setValue("778210003");
-    Xrm.Page.data.entity.save();
-  }
-  if (fase === "Facturación Parcial") {
-    estado.setValue(778210004);
-    estate.setValue("778210004");
-    Xrm.Page.data.entity.save();
-  }
-  if (fase === "Facturado") {
-    estado.setValue(100003);
-    estate.setValue("100003");
-    Xrm.Page.data.entity.save();
-  }
-  console.log("Estado Pedido: " + estado.getValue());
-  console.log(estate.getValue());
+  try {
+    let formContext = executeContext.getFormContext();
+    var objEstado = formContext.data.process.getActiveStage();
+    var estado = formContext.getAttribute("statuscode");
+    var estate = formContext.getAttribute("billto_line3");
+    var statecode = formContext.getAttribute("statecode");
+    var fase = objEstado.getName();
+    console.clear();
+    //alert("JS Estado Pedido: Fase = "+fase);
+    console.log("JS Estado Pedido: Fase = " + fase);
 
+    if (fase === "En Elaboración") {
+      estado.setValue(1);
+      estate.setValue("1");
+      Xrm.Page.data.entity.save();
+    }
+    if (fase === "En Revisión") {
+      estado.setValue(2);
+      estate.setValue("2");
+      Xrm.Page.data.entity.save();
+    }
+    if (fase === "Área Operativa") {
+      estado.setValue(778210005);
+      estate.setValue("778210005");
+      Xrm.Page.data.entity.save();
+    }
+    if (fase === "Pendiente Por Compra" || fase === "Pendiente por compra") {
+      estado.setValue(778210000);
+      estate.setValue("778210000");
+      Xrm.Page.data.entity.save();
+    }
+    if (fase === "Detenido En Compras" || fase === "Detenido en compras") {
+      estado.setValue(778210001);
+      estate.setValue("778210001");
+      Xrm.Page.data.entity.save();
+    }
+    if (fase === "Pendiente Por Recibir" || fase === "Pendiente por recibir") {
+      estado.setValue(778210002);
+      estate.setValue("778210002");
+      Xrm.Page.data.entity.save();
+    }
+    if (fase === "Pendiente Por Facturar" || fase === "Pendiente por facturar") {
+      estado.setValue(778210003);
+      estate.setValue("778210003");
+      Xrm.Page.data.entity.save();
+    }
+    if (fase === "Facturación Parcial") {
+      estado.setValue(778210004);
+      estate.setValue("778210004");
+      Xrm.Page.data.entity.save();
+    }
+    if (fase === "Facturado") {
+      estado.setValue(100003);
+      estate.setValue("100003");
+      Xrm.Page.data.entity.save();
+    }
+    console.log("Estado Pedido: " + estado.getValue());
+    console.log(estate.getValue());
+
+
+  } catch (error) {
+    console.log(error);
+    console.log("error script estadoPedido.js");
+  }
   //alert("JS Estado Pedido: "+estado.getValue());
 }
 
-try {
-
-} catch (error) {
-
-}
