e370c56d7d8a35f88d8b856fa1955c7f2eb10709
Sergio redondo <sredondo95@gmail.com>
2021-10-05 12:10:06 -0500

    guardado pc empresa 05-10-21

--- incident_validar_fechadeatencion_y_finrealdeatencion.js
+++ incident_validar_fechadeatencion_y_finrealdeatencion.js
@@ -1,37 +1,36 @@
-
-
-
 function validarFechas(executionContext) {
+  let formContext = executionContext.getFormContext();
 
+  let ap_iniciorealdeatencion = formContext.getAttribute(
+    "ap_iniciorealdeatencion"
+  );
+  let ap_finrealdeatencion = formContext.getAttribute("ap_finrealdeatencion");
 
+  if (ap_iniciorealdeatencion.getValue() && ap_finrealdeatencion.getValue()) {
+    console.log(ap_finrealdeatencion.getValue().getTime());
+    console.log(ap_iniciorealdeatencion.getValue().getTime());
 
-    let formContext = executionContext.getFormContext();
+    if (
+      ap_finrealdeatencion.getValue().getTime() <
+      ap_iniciorealdeatencion.getValue().getTime()
+    ) {
+      var alertStrings = {
+        confirmButtonLabel: "Yes",
+        text: "La fecha final de atencion no puede ser menor  a la fecha inicial de atencion. se le colocara la fecha actual",
+      };
+      var alertOptions = { height: 120, width: 260 };
 
-    let ap_iniciorealdeatencion = formContext.getAttribute("ap_iniciorealdeatencion");
-    let ap_finrealdeatencion = formContext.getAttribute("ap_finrealdeatencion");
-
-
-    if (ap_iniciorealdeatencion.getValue() && ap_finrealdeatencion.getValue()) {
-
-        console.log(ap_finrealdeatencion.getValue().getTime());
-        console.log(ap_iniciorealdeatencion.getValue().getTime());
-
-        if (ap_finrealdeatencion.getValue().getTime() < ap_iniciorealdeatencion.getValue().getTime()) {
-
-            var alertStrings = { confirmButtonLabel: 'Yes', text: 'La fecha final de atencion no puede ser menor  a la fecha inicial de atencion' };
-            var alertOptions = { height: 120, width: 260 };
-
-            Xrm.Navigation.openAlertDialog(alertStrings, alertOptions).then(
-                function success() {
-                    // perform operations on alert dialog close
-                    ap_finrealdeatencion.setValue(null);
-                },
-                function (error) {
-                    console.log(error.message);
-                    // handle error conditions
-                }
-            );
-
+      Xrm.Navigation.openAlertDialog(alertStrings, alertOptions).then(
+        function success() {
+          // perform operations on alert dialog close
+          ap_finrealdeatencion.setValue(null);
+          ap_finrealdeatencion.setValue(new Date());
+        },
+        function (error) {
+          console.log(error.message);
+          // handle error conditions
         }
+      );
     }
+  }
 }
