76accc17b3dcbe9db38a234464da9f4c9896d4e3
Serch <sredondo95@gmail.com>
2021-12-02 17:59:57 -0500

    finished 02/12/2021

--- incident_validar_fechadeatencion_y_finrealdeatencion.js
+++ incident_validar_fechadeatencion_y_finrealdeatencion.js
@@ -1,36 +1,48 @@
 function validarFechas(executionContext) {
-  let formContext = executionContext.getFormContext();
+    let formContext = executionContext.getFormContext();
 
-  let ap_iniciorealdeatencion = formContext.getAttribute(
-    "ap_iniciorealdeatencion"
-  );
-  let ap_finrealdeatencion = formContext.getAttribute("ap_finrealdeatencion");
+    let ap_iniciorealdeatencion = formContext.getAttribute(
+        "ap_iniciorealdeatencion"
+    );
+    let ap_finrealdeatencion = formContext.getAttribute("ap_finrealdeatencion");
+    let todayDate = new Date();
+    const mensajeI =
+        "La fecha inicial de atencion no puede ser menor a la fecha actual";
+    const mensajeF =
+        "La fecha final de atencion no puede ser menor  a la fecha inicial de atencion. se le colocara la fecha actual";
 
-  if (ap_iniciorealdeatencion.getValue() && ap_finrealdeatencion.getValue()) {
-    console.log(ap_finrealdeatencion.getValue().getTime());
-    console.log(ap_iniciorealdeatencion.getValue().getTime());
-
-    if (
-      ap_finrealdeatencion.getValue().getTime() <
-      ap_iniciorealdeatencion.getValue().getTime()
-    ) {
-      var alertStrings = {
-        confirmButtonLabel: "Yes",
-        text: "La fecha final de atencion no puede ser menor  a la fecha inicial de atencion. se le colocara la fecha actual",
-      };
-      var alertOptions = { height: 120, width: 260 };
-
-      Xrm.Navigation.openAlertDialog(alertStrings, alertOptions).then(
-        function success() {
-          // perform operations on alert dialog close
-          ap_finrealdeatencion.setValue(null);
-          ap_finrealdeatencion.setValue(new Date());
-        },
-        function (error) {
-          console.log(error.message);
-          // handle error conditions
-        }
-      );
+    if (ap_iniciorealdeatencion.getValue() && !ap_finrealdeatencion.getValue()) {
+        if (ap_iniciorealdeatencion.getValue().getTime() > todayDate.getTime())
+            mensajeAlerta(executionContext, mensajeI, "ap_iniciorealdeatencion");
     }
-  }
+
+    if (ap_iniciorealdeatencion.getValue() && ap_finrealdeatencion.getValue()) {
+        if (
+            ap_finrealdeatencion.getValue().getTime() <
+            ap_iniciorealdeatencion.getValue().getTime()
+        ) {
+            mensajeAlerta(executionContext, mensajeF, "ap_finrealdeatencion");
+        }
+    }
+}
+
+function mensajeAlerta(executionContext, mensaje, campo) {
+    let formContext = executionContext.getFormContext();
+    var alertStrings = {
+        confirmButtonLabel: "Yes",
+        text: mensaje,
+    };
+    var alertOptions = { height: 120, width: 260 };
+
+    Xrm.Navigation.openAlertDialog(alertStrings, alertOptions).then(
+        function success() {
+            // perform operations on alert dialog close
+            formContext.getAttribute(campo).setValue(null);
+            formContext.getAttribute(campo).setValue(new Date());
+        },
+        function(error) {
+            console.log(error.message);
+            // handle error conditions
+        }
+    );
 }
